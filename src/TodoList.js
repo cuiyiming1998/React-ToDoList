f2cc136d2a2ce92e4ae2db4d35a45afeab2d5665
cuiyiming1998 <1269567664@qq.com>
2019-04-03 14:09:12 +0800

    3nd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,78 +1,53 @@
-import React, { Component,Fragment } from 'react';
-import './App.css';
-import TodoItem from './TodoItem';
+import React, { Component } from 'react'
+import store from './store';
+import { connect } from 'react-redux';
+//2019.3.28
 
-// TodoList知识点：
-// js的this绑定，写在dom上的方法this会指向dom元素，需要用bind绑定this到class
-// 父组件通过参数的形式向子组件传递参数
-// 子组件通过props接收父组件传递过来的参数
-// 父子组件通信，子组件通过父组件传递过来的方法，触发父组件上绑定的方法，达到通信的目的
-
-class TodoList extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      inputValue: ''
+export default class TodoList extends Component {
+    constructor(){
+        super();
+        this.state = {
+            list:store.getState().todo.list
+        }
+        console.log(this.state.list)
+        store.subscribe(()=>{//改变就更新
+            this.setState({
+                list:store.getState().todo.list
+            })
+        })
     }
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
-  }
-
-  handleBtnClick(e) {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
-    })
-  }
-
-  handleItemClick(index) {
-    console.log(index);
-    const list = [...this.state.list];
-    list.splice(index,1);
-    this.setState({
-      list
-    })
-  }
-
-  // 父组件通过属性的形式向子组件传递参数
-  // 子组件通过props接收父组件传递过来的参数
-
-  handleDelete(index) {
-    console.log(index);
-    const list = [...this.state.list];
-    list.splice(index,1);
-    this.setState({
-      list
-    })
-  }
-
-  getTodoItem() {
-    return this.state.list.map((item,index)=>{
-      return (
-        <TodoItem deleteItem={this.handleDelete} key={index} content={item} index={index} />
-      )
-    })
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <input value={this.state.inputValue} onChange={this.handleInputChange} />
-        <button className="red-btn" onClick={this.handleBtnClick}>Add!</button>
-        <ul>{this.getTodoItem()}</ul>
-      </Fragment>
-    );
-  }
+    handleAdd=(e)=>{
+        if (e.which !== 13) return;
+        // if (e.keyCode === 13) {
+        store.dispatch({
+            type: 'add_item',
+            value:e.target.value
+        })
+        // }
+        e.target.value=''
+    }
+    handleDec=(index)=>{
+        // console.log(index);
+        store.dispatch({
+            type: 'dec_item',
+            value: index
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input onKeyPress={(e)=>{this.handleAdd(e)}} type="text"/><br/>
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>(
+                            <li>
+                                <span>{item}</span>
+                                <input key={index} type="button" value="删除" onClick={()=>{this.handleDec(index)}}/>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
-
-export default TodoList;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,34 @@
-const defaultState = {
-    inputValue: '',
-    list: [1,2],
-    showMsg: false
+import { combineReducers } from 'redux';
+
+let counter=(state=0,action)=>{//纯函数 (state,action) state是新的 action返回
+    switch(action.type){
+        case 'ADD':
+            return state + action.value;//原来的＋1
+        case 'DEC':
+            return state - action.value;
+        default:
+            return state;
+    }
 }
 
-export default (state = defaultState,action)=>{
-    return state;
+let initValue = {
+    list:[1,2,3]
 }
+let todo=(state=initValue,action)=>{//纯函数 (state,action) state是新的 action返回
+    switch(action.type){
+        case 'add_item':
+            let newState = JSON.parse(JSON.stringify(state))//方法1
+            // let obj = Object.assign({},state) //方法2
+            newState.list.push(action.value);
+            return newState;
+        case 'dec_item':
+            let deleteState = JSON.parse(JSON.stringify(state));
+            deleteState.list.splice(action.index,1);
+            return deleteState;
+        default:
+            return state;
+    }
+}
+export default combineReducers({
+    counter,todo
+})
